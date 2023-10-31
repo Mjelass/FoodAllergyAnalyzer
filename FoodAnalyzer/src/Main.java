4a1388950256c770e1128b21985630f6d85cfd7f
Mehdi Jelassia <108407452+Mjelass@users.noreply.github.com>
2023-10-31 14:30:51 +0000

    backbone0.0.0

---
+++ FoodAnalyzer/src/Main.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * Starts the console UI for the CoffeeMaker
+ */
+public class Main {
+	/**
+	 * Passes a prompt to the user and returns the user specified 
+	 * string.
+	 * @param message
+	 * @return int
+	 */
+	private static int inputOutput(String message) {
+		System.out.println(message);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int returnValue = 0;
+		try {
+			returnValue = Integer.parseInt(br.readLine());
+		} catch (NumberFormatException e) {
+			System.out.println("Error reading in integer value");
+			mainMenu();
+		}
+		catch (IOException e){
+			System.out.println("Error reading in value");
+			mainMenu();
+		}
+		return returnValue;
+	}
+
+
+
+
+
+	/**
+	 * Prints the main menu and handles user input for 
+	 * main menu commands.
+	 */
+	public static void mainMenu() {
+		System.out.println("1. Check list of all food and Allergies associated");
+		System.out.println("2. Check list of food suitable with my Allergies");
+		System.out.println("0. Exit\n");
+
+		//Get user input
+		int userInput = inputOutput("Please press the number that corresponds to what you would like the food analyzer to do.");
+
+		if (userInput >= 0 && userInput <=2) {
+			//if (userInput == 1) AllListFoodAllergy();
+			//if (userInput == 2) FoodSuitableAllergy();
+			if (userInput == 0) System.exit(0);
+		} else {
+			System.out.println("Please enter a number from 0 - 4");
+			mainMenu();
+		}
+	}
+
+	/**
+	 * Starts the coffee maker program.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to the FoodAnalyzer!\n");
+		mainMenu();
+	}
+}
