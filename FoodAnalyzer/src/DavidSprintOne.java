4cb9a946484f6dc84f7fac01112ef151d54b9b05
Papalpatine <davidloisel2001@gmail.com>
2023-11-10 08:44:15 +0000

    Creating categories

--- FoodAnalyzer/src/DavidSprintOne.java
+++ FoodAnalyzer/src/DavidSprintOne.java
@@ -1,18 +1,87 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class DavidSprintOne {
+	
+	public static class Categories {
+	        List<String> Bakery = new ArrayList<>();
+	        List<String> Meat = new ArrayList<>();
+	        List<String> Drinks = new ArrayList<>();
+	        List<String> FruitsAndVegetables = new ArrayList<>();
+	        List<String> Others = new ArrayList<>();
+	        
+	        public Categories() {
+	        Bakery.add("Bread");//Wheat
+	        Bakery.add("ChocolateCake");//milk and eggs 
+	        Bakery.add("Biscuits");//milk and eggs 
+	        Bakery.add("Cereals");//Wheat
+	        Meat.add("Sausages");
+	    	Meat.add("Vegan Meat");//soy
+	    	Meat.add("Fish");//fish
+	    	Meat.add("Ham");
+	    	Drinks.add("Water");
+	    	Drinks.add("Beer");//wheat
+	    	Drinks.add("Coffee");
+	    	Drinks.add("HotChocolate");//milk and peanuts
+	    	FruitsAndVegetables.add("Banana");
+	    	FruitsAndVegetables.add("Strawberries");//strawberries
+	    	FruitsAndVegetables.add("Avocado");//avocado
+	    	FruitsAndVegetables.add("Potatoes");//potatoes
+	    	Others.add("Pasta");//sesame and wheat
+	    	Others.add("Eggs");//eggs
+	    	Others.add("Rice");
+	    	Others.add("Mustard");//mustard
+	    }
+
 	/**
 	 * Input a number and look up a list of categories
 	 * @return list of categories
 	 */
-	public String[] lookUpCatergoriesList() {
-		return null;
+	public Categories lookUpCategoriesList(int number) {
+		System.out.println("Please choose a topic :" );
+		System.out.println("1) Search allergies by food" );
+		System.out.println("2) Search foods by my allergies" );
+		System.out.println("3) Look up account" );
+		System.out.println("4) ??" );
+		System.out.println("0) Go back" );
+		switch (number) {
+        case 0:
+            return null;
+        case 1:
+        	return new Categories();
+		}
 	}
+	
 	/**
 	 * Input a number and look up a list of Foods of a category
 	 * @param Number
 	 * @return list of food based on number
 	 */
-	public Food[] lookUpFoodsList(int Number) {
-		return null;
+	public List<String> lookUpFoodsList(int choice) {
+		System.out.println("Please choose a category :" );
+		System.out.println("1) Bakery" );
+		System.out.println("2) Meat" );
+		System.out.println("3) Drinks" );
+		System.out.println("4) Fruits And Vegetables" );
+		System.out.println("5) Others" );
+		System.out.println("0) Go back" );
+		switch (choice) {
+		case 0 : 
+			return null;
+		case 1:
+            return Bakery;
+        case 2:
+            return Meat;
+        case 3:
+            return Drinks;
+        case 4:
+            return FruitsAndVegetables;
+        case 5:
+            return Others;
+        default:
+            return new ArrayList<>();
+		}
 	}
 	/**
 	 * Input a number to choose a Food among the list
@@ -36,6 +105,7 @@ public class DavidSprintOne {
 	 * @return list of foods based on my list of allergies
 	 */
 	public Food[] checkingFoodsByAllergies(String[] MyAllergies) {
+		//return list of foods available for my allergies 
 		return null;
 	}
 }
