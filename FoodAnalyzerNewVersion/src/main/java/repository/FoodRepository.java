479d8ba3fbb54845734f96d1e432714cf361f74e
Mehdi Jelassia <108407452+Mjelass@users.noreply.github.com>
2023-11-16 17:00:43 +0000

    Delete FoodAnalyzerNewVersion directory

--- FoodAnalyzerNewVersion/src/main/java/repository/FoodRepository.java
+++
@@ -1,8 +0,0 @@
-package main.java.repository;
-import java.util.List;
-
-import main.java.model.Food;
-
-public interface FoodRepository {
-}
-
--- FoodAnalyzerNewVersion/src/main/java/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package main.java.repository;
-import main.java.model.User;
-
-public interface UserRepository {
-    
-    void addUser(User newUser);
-    
-}
