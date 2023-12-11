541dc68ef7f9ccee710adf309a6c8bca4e247332
Rohit-V10 <137171783+Rohit-V10@users.noreply.github.com>
2023-12-12 01:25:59 +0400

    Add files via upload

--- FoodAnalyzerNewVersion/Tests/Black_Box/Specification/Food_API_SpecificationTest.java
+++ FoodAnalyzerNewVersion/Tests/Black_Box/Specification/Food_API_SpecificationTest.java
@@ -9,8 +9,9 @@ import main.java.repository.Food_API;
 public class Food_API_SpecificationTest {
 
     private static class MockFoodAPI extends Food_API {
+        // Override the sendHttpRequest method to mock its behavior
+        @Override
         public String sendHttpRequest(String apiUrl, int startIndex) throws Exception {
-            // Mock the behavior of sendHttpRequest
             return "Mocked Allergens Response";
         }
     }
@@ -18,11 +19,22 @@ public class Food_API_SpecificationTest {
     @Test
     public void testMainMethod_ReturnsCorrectAllergensList() {
         try {
+            // Redirect standard output to capture the printed content
+            final var originalOut = System.out;
+            final var mockOut = new java.io.ByteArrayOutputStream();
+            System.setOut(new java.io.PrintStream(mockOut));
+
             // Create an instance of MockFoodAPI
             MockFoodAPI mockAPI = new MockFoodAPI();
 
             // Run the main method
-            String result = mockAPI.main(null);
+            mockAPI.main(null);
+
+            // Reset standard output
+            System.setOut(originalOut);
+
+            // Get the captured output
+            String result = mockOut.toString();
 
             // Perform assertions on the returned values
             assertTrue(result.contains("List of codes for allergens"));
